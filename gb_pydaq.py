9c85d42caea7cf8b2b79fc689dc060834f887611
maria <parody@uw.edu>
2013-10-22 16:11:48 -0700

    making an object for giving reward

--- gb_pydaq.py
+++ gb_pydaq.py
@@ -2,55 +2,53 @@ import PyDAQmx as daq
 import numpy as np
 import time
 
-# set up reward object. sends reward pulse to digital output on daq
-reward = daq.Task()
+class GiveReward(daq.Task):
+    def __init__(self):
+        daq.Task.__init__(self)
+        self.pulse = np.zeros(1, dtype = np.uint8)
+        self.CreateDOChan("Dev1/port0/line0", "", daq.DAQmx_Val_ChanPerLine)
+        self.StartTask()
 
-reward.CreateDOChan("Dev1/port0/line0", "", daq.DAQmx_Val_ChanPerLine)
-reward.StartTask()
-pumpWrite = np.zeros(1, dtype=np.uint8)
-pumpWrite[0] = 1
-reward.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
-                                pumpWrite, None, None)
-pumpWrite[0] = 0
-time.sleep(.05)
-reward.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
-                                pumpWrite, None, None)
-print "sent reward impulse"
+    def pumpout(self):
+        self.pulse[0] = 1
+        self.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
+                                 self.pulse, None, None)
+        self.pulse[0] = 0
+        time.sleep(.05)
+        self.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
+                                 self.pulse, None, None)
+        print "sent reward impulse"
+
+#task = GiveReward()
+#task.pumpout()
+
+# set up reward object. sends reward pulse to digital output on daq
+#reward = daq.Task()
 #
-#try:
-#    daq.DAQmxCreateTask("", daq.byref(pumpTaskHandle))
-#    daq.DAQmxCreateDOChan(pumpTaskHandle, "Dev1/port0/line0", "", daq.DAQmx_Val_ChanPerLine)
-#
-#    daq.DAQmxStartTask(pumpTaskHandle)
-#
-#    pumpWrite = np.zeros(1, dtype=np.uint8)
-#    pumpWrite[0] = 1
-#    daq.DAQmxWriteDigitalLines(pumpTaskHandle, 1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
-#                           pumpWrite, None, None)
-#    pumpWrite[0] = 0
+#reward.CreateDOChan("Dev1/port0/line0", "", daq.DAQmx_Val_ChanPerLine)
+##reward.CreateDOChan("sim-pci-6221/port0/line0", "", daq.DAQmx_Val_ChanPerLine)
+#reward.StartTask()
+#def giveReward(self):
+#    reward.pumpWrite = np.zeros(1, dtype=np.uint8)
+#    reward.pumpWrite[0] = 1
+#    reward.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
+#                             reward.pumpWrite, None, None)
+#    reward.pumpWrite[0] = 0
 #    time.sleep(.05)
-#    daq.DAQmxWriteDigitalLines(pumpTaskHandle, 1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
-#                           pumpWrite, None, None)
+#    reward.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
+#                             reward.pumpWrite, None, None)
 #    print "sent reward impulse"
 #
-#except daq.DAQError as err:
-#    print "DAQmx Error: %s"%err
+## set up eog
+#eogSampRate = 240
+#eogSampsPerChanToAcquire = 1
+#eog = daq.Task()
+#eogData = np.zeros(2)
 #
-#finally:
-#    if pumpTaskHandle:
-#        daq.DAQmxStopTask(pumpTaskHandle)
-#        daq.DAQmxClearTask(pumpTaskHandle)
-
-# Stuff for writing to file on other machine?
-#pEncode = np.zeros(1, dtype=numpy.uint32)
-#pEncodeTaskHandle = TaskHandle()
+#eog.CreateAIVoltageChan("Dev1/ai3", "", daq.DAQmx_Val_RSE, -5.0, 5.0, daq.DAQmx_Val_Volts, None)
+#eog.CreateAIVoltageChan("Dev1/ai4", "", daq.DAQmx_Val_RSE, -5.0, 5.0, daq.DAQmx_Val_Volts, None)
+#eog.CfgSampClkTiming("", eogSampRate, daq.DAQmx_Val_Rising, daq.DAQmx_Val_ContSamps, eogSampsPerChanToAcquire)
 #
-#try:
-#    DAQmxCreateTask("",byref(pEncodeTaskHandle))
-#    DAQmxCreateDOChan( pEncodeTaskHandle, "Dev1/port2","",DAQmx_Val_ChanForAllLines)
-#
-#    DAQmxStartTask(pEncodeTaskHandle)
-#pEncode[0] = event
-#DAQmxWriteDigitalU32(pEncodeTaskHandle, 1, 0, 10.0,
-#                     DAQmx_Val_GroupByChannel, pEncode, byref(int32()),
-#                     None)
+#eog.ReadAnalogF64(1,10.0,daq.DAQmx_Val_GroupByScanNumber,eogData,2,daq.byref(daq.read),None)
+#eog.StartTask()
+#eog.StopTask()
