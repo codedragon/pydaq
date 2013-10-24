9be4addc5d4619a4d2a5eb96fcdbdcd0143a2003
maria <parody@uw.edu>
2013-10-23 17:33:04 -0700

    pydaq_object successfully collects data from ai3 or ai4, gb_pydaq not so much

--- gb_pydaq.py
+++ gb_pydaq.py
@@ -2,56 +2,63 @@ import PyDAQmx as daq
 import numpy as np
 import time
 
+
 class GiveReward(daq.Task):
     def __init__(self):
         daq.Task.__init__(self)
-        self.pulse = np.zeros(1, dtype = np.uint8)
+        self.pulse = np.zeros(1, dtype=np.uint8)
         self.CreateDOChan("Dev1/port0/line0", "", daq.DAQmx_Val_ChanPerLine)
         self.StartTask()
 
     def pumpOut(self):
         self.pulse[0] = 1
         self.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
-                                 self.pulse, None, None)
+                               self.pulse, None, None)
         self.pulse[0] = 0
         time.sleep(.05)
         self.WriteDigitalLines(1, False, daq.DAQmx_Val_WaitInfinitely, daq.DAQmx_Val_GroupByChannel,
-                                 self.pulse, None, None)
+                               self.pulse, None, None)
         print "sent reward impulse"
 
 #task = GiveReward()
 #task.pumpOut()
-
-class eog(daq.Task):
-
+#
+class eogTask(daq.Task):
     def __init__(self):
         daq.Task.__init__(self)
-        self.eogSampRate = 240
-        self.eogSampsPerChanToAcquire = 1
-        self.eogData = np.zeros(0)
+        eogSampRate = 240
+        eogSampsPerChanToAcquire = 1
+        self.eogData = np.zeros((0), dtype=np.float64)
         self.CreateAIVoltageChan("Dev1/ai3", "", daq.DAQmx_Val_RSE, -5.0, 5.0, daq.DAQmx_Val_Volts, None)
         self.CreateAIVoltageChan("Dev1/ai4", "", daq.DAQmx_Val_RSE, -5.0, 5.0, daq.DAQmx_Val_Volts, None)
-        self.CfgSampClkTiming("", self.eogSampRate, daq.DAQmx_Val_Rising, daq.DAQmx_Val_ContSamps, self.eogSampsPerChanToAcquire)
+        self.CfgSampClkTiming("", eogSampRate, daq.DAQmx_Val_Rising, daq.DAQmx_Val_ContSamps, eogSampsPerChanToAcquire)
+        self.AutoRegisterEveryNSamplesEvent(daq.DAQmx_Val_Acquired_Into_Buffer, 1000, 0)
+        self.AutoRegisterDoneEvent(0)
+        print 'init done'
 
     def EveryNCallback(self):
-        read = np.int32()
+        print 'callback'
+        read = daq.int32()
+        print 'read', read
         self.ReadAnalogF64(1, 10.0, daq.DAQmx_Val_GroupByScanNumber, self.eogData, 2, daq.byref(read), None)
         print 'x,y', self.eogData[0], self.eogData[1]
         print 'okay'
         return 0  # the function should return an integer
 
     def DoneCallback(self, status):
-       print 'Status', status.value
-       print 'what'
-       return 0  # the function should return an integer
+        print 'done callback'
+        print 'Status', status.value
+        print 'what'
+        return 0  # the function should return an integer
 
-task = eog()
+
+task = eogTask()
 task.StartTask()
 
 raw_input('Acquiring samples continuously. Press Enter to interrupt\n')
 
 task.StopTask()
-task.ClearTask
+task.ClearTask()
 
 #
 ## set up eog
@@ -59,12 +66,13 @@ task.ClearTask
 #eogSampsPerChanToAcquire = 1
 #eog = daq.Task()
 #eogData = np.zeros(2)
+#read = np.int32()
 #
 #eog.CreateAIVoltageChan("Dev1/ai3", "", daq.DAQmx_Val_RSE, -5.0, 5.0, daq.DAQmx_Val_Volts, None)
 #eog.CreateAIVoltageChan("Dev1/ai4", "", daq.DAQmx_Val_RSE, -5.0, 5.0, daq.DAQmx_Val_Volts, None)
 #eog.CfgSampClkTiming("", eogSampRate, daq.DAQmx_Val_Rising, daq.DAQmx_Val_ContSamps, eogSampsPerChanToAcquire)
 #
-#eog.ReadAnalogF64(1,10.0,daq.DAQmx_Val_GroupByScanNumber,eogData,2,daq.byref(daq.read),None)
+#eog.ReadAnalogF64(1, 10.0, daq.DAQmx_Val_GroupByScanNumber, eogData, 2, daq.byref(read), None)
 #eog.StartTask()
 #eog.StopTask()
 
--- pydaq_object.py
+++ pydaq_object.py
@@ -6,7 +6,7 @@ read = int32()
 data = numpy.zeros((1000,), dtype=numpy.float64)
 
 # DAQmx Configure Code
-analog_input.CreateAIVoltageChan("Dev1/ai0","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None)
+analog_input.CreateAIVoltageChan("Dev1/ai4","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None)
 analog_input.CfgSampClkTiming("",10000.0,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,1000)
 
 # DAQmx Start Code
@@ -16,3 +16,4 @@ analog_input.StartTask()
 analog_input.ReadAnalogF64(1000,10.0,DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
 
 print "Acquired %d points"%read.value
+print data
