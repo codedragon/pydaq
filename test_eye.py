b19448ca149bab68dcd9d2ed24455eeaa18cef83
Maria <parody@uw.edu>
2015-09-10 12:42:02 -0700

    changed test_eye so looking at both channels

--- test_eye.py
+++ test_eye.py
@@ -10,14 +10,21 @@ data = numpy.zeros((1000,), dtype=numpy.float64)
 
 # DAQmx Configure Code
 # we use Dev1/ai3 and Dev1/ai4 for eye tracking computer
+analog_input.CreateAIVoltageChan("Dev1/ai3","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None)
 analog_input.CreateAIVoltageChan("Dev1/ai4","",DAQmx_Val_Cfg_Default,-10.0,10.0,DAQmx_Val_Volts,None)
-analog_input.CfgSampClkTiming("",10000.0,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,1000)
+analog_input.CfgSampClkTiming("",10000.0,DAQmx_Val_Rising,DAQmx_Val_FiniteSamps,500)
 
 # DAQmx Start Code
 analog_input.StartTask()
 
 # DAQmx Read Code
-analog_input.ReadAnalogF64(1000,10.0,DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
+#analog_input.ReadAnalogF64(500,10.0,DAQmx_Val_GroupByChannel,data,1000,byref(read),None)
+analog_input.ReadAnalogF64(500,10.0,DAQmx_Val_GroupByScanNumber,data,1000,byref(read),None)
+
+analog_input.StopTask()
+analog_input.ClearTask()
 
 print "Acquired %d points"%read.value
 print data
+print data.size
+
